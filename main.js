2215dc788dee891f9d55a6f9a7064271bab4bf0f
joncwchao <joncwchao@gmail.com>
2021-01-07 22:02:36 -0800

    Begin porting cards

--- main.js
+++ main.js
@@ -1,65 +1,70 @@
-var cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-var suits = ["diamonds", "hearts", "spades", "clubs"];
-var deck = new Array();
-
-function getDeck()
+function draw()
 {
-	var deck = new Array();
-
-	for(var i = 0; i < suits.length; i++)
-	{
-		for(var x = 0; x < cards.length; x++)
-		{
-			var card = {Value: cards[x], Suit: suits[i]};
-			deck.push(card);
-		}
-	}
-
-	return deck;
-}
-
-function shuffle()
-{
+	var random = Math.floor(Math.random() * cards.length);
 	// for 1000 turns
 	// switch the values of two random cards
-	for (var i = 0; i < 1000; i++)
-	{
-		var location1 = Math.floor((Math.random() * deck.length));
-		var location2 = Math.floor((Math.random() * deck.length));
-		var tmp = deck[location1];
-
-		deck[location1] = deck[location2];
-		deck[location2] = tmp;
-	}
-
-	renderDeck();
+	console.log(`got random ${random}`);
+	renderCard(cards[random]);
 }
 
-function renderDeck()
+function renderCard(card)
 {
-	document.getElementById('deck').innerHTML = '';
-	for(var i = 0; i < deck.length; i++)
+	console.log(`card value = ${card}`);
+	document.getElementById("card").innerHTML = 
+			`<b>Easy</b></br></hr></br>${card.easy}</br></hr></br>` +
+			`<b>Medium</b></br></hr></br>${card.medium}</br></hr></br>` +
+			`<b>Hard</b></br></hr></br>${card.hard}</br></hr></br>`;
+}
+
+var cards = [
 	{
-		var card = document.createElement("div");
-		var value = document.createElement("div");
-		var suit = document.createElement("div");
-		card.className = "card";
-		value.className = "value";
-		suit.className = "suit " + deck[i].Suit;
-
-		value.innerHTML = deck[i].Value;
-		card.appendChild(value);
-		card.appendChild(suit);
-
-		document.getElementById("deck").appendChild(card);
-	}
-}
-
-function load()
-{
-	deck = getDeck();
-	shuffle();
-	renderDeck();
-}
-
-window.onload = load;
+		easy: ["Restaurant</br>Style</br>Mole"],
+		medium: ["Prosthesis</br>Leaves of Grass</br>Maggie Gyllenhaal"],
+		hard: ["Resident Evil</br>Game of Thrones</br>Statue of Liberty"]
+	},
+	{
+		easy: ["Crocodile</br>Canary</br>Piano"],
+		medium:["Target</br>Billy the Kid</br>Moustache"],
+		hard: ["Adam Carolla</br>Footbath</br>Not for the faint-hearted"]
+	},
+	{
+		easy: ["Fork</br>Gun</br>Bat"],
+		medium:["Forrest Gump</br>Ferret</br>Cockroach"],
+		hard: ["Know your limits</br>Disappear into thin air</br>Heartbreaking"]
+	},
+	{
+		easy: ["Little House on the Prairie</br>Public holiday</br>Volcano"],
+		medium:["Tinkerbell</br>Julia Roberts</br>Whiskey"],
+		hard: ["The Nightmare Before Christmas</br>Boys will be boys</br>Sell your soul to the devil"]
+	},
+	{
+		easy: ["Bank</br>Bedroom</br>Flute"],
+		medium:["Archimedes</br>Borat</br>Fast and Furious"],
+		hard: ["To be given free rein</br>Billy Jean</br>Eureka!"]
+	},
+	{
+		easy: ["Wallet</br>Tin</br>Diamond"],
+		medium:["Beach</br>Lawyer</br>Cookie"],
+		hard: ["Fools rush in</br>I smell a rat</br>From the Earth to the Moon"]
+	},
+	{
+		easy: ["Meteorite</br>Museum</br>Refrigerator"],
+		medium:["Contact lens</br>The Sims</br>Power Rangers"],
+		hard: ["The Age of Enlightenment</br>Clammy hands</br>Rain Man"]
+	},
+	{
+		easy: ["Printer</br>Chalet</br>Carpet"],
+		medium:["John Lennon</br>Bowling for Columbine</br>Snowboard"],
+		hard: ["Ted Koppel</br>Big Ben</br>Skyfall"]
+	},
+	{
+		easy: ["Tepee</br>Shirt</br>Rocket"],
+		medium:["Sushi</br>Calculator</br>French fries"],
+		hard: ["Tina Turner</br>Brood over</br>Fight Club"]
+	},
+	{
+		easy: ["SpongeBob SquarePants</br>Spice</br>Tarzan"],
+		medium:["Gone With The Wind</br>Ocean's Eleven</br>Crop circle"],
+		hard: ["Hungover</br>Sleep with one eye open</br>Snoopy"]
+	},
+];
